a1a2972b1b1f3af3e444c7c54b640e691c38d59d
Hajime Fujita <hajime.fujita@gmail.com>
2020-12-13 13:09:24 +0000

    init

---
+++ mysql/init.d/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS octave_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER IF NOT EXISTS 'octave_user'@'%' IDENTIFIED BY 'octave';
+GRANT ALL PRIVILEGES ON octave_db.* TO 'octave_user'@'%';
+FLUSH PRIVILEGES;
