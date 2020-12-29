afb6dca0b03423b6df62b6e68472952a64e7683d
Hajime Fujita <hajime.fujita@gmail.com>
2020-12-29 14:55:48 +0000

    少し変更

--- src/users/admin.py
+++ src/users/admin.py
@@ -3,28 +3,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-# class UserModelAdmin(admin.ModelAdmin):
-#     #ordering('-created_at',)
-#     #readonly_fields = ('id', 'uuid', 'created_at',)
-
-#     def change_view(self, request, object_id, form_url='', extra_context=None):
-#         self.readonly_fields = ('id', 'uuid', 'created_at',)
-#         return self.changeform_view(request, object_id, form_url, extra_context)
-
-#     def add_view(self, request, form_url='', extra_context=None):
-#         self.readonly_fields = ()
-#         return self.changeform_view(request, None, form_url, extra_context)    
-
 @admin.register(User)
 class AdminUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('last_name', 'first_name', 'email')}),
+        (_('Personal info'), {'fields': ('last_name', 'first_name', 'last_name_kana', 'first_name_kana', 'old_last_name', 'old_last_name_kana', 'email', 'sex', 'birthday', 'postal_code', 'prefecture', 'address', 'building', 'tel', 'url', 'photo')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    list_display = ('username', 'email', 'is_staff')
-    search_fields = ('username', 'email')
+    list_display = ('get_full_name', 'get_full_name_kana', 'email', 'sex', 'birthday', 'postal_code', 'prefecture', 'address', 'building', 'tel', 'is_staff',)
+    search_fields = ('username', 'email',)
+    ordering = ('date_joined',)
     filter_horizontal = ('groups', 'user_permissions')
--- src/users/models.py
+++ src/users/models.py
@@ -10,7 +10,7 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -42,7 +42,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
         _('username'),
         max_length=150,
-        unique=True,
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
         validators=[username_validator],
         error_messages={
@@ -50,10 +49,27 @@ class User(AbstractBaseUser, PermissionsMixin):
         },
     )
     
-    first_name = models.CharField(_('first name'), max_length=150)
-    last_name = models.CharField(_('last name'), max_length=150)
-    email = models.EmailField(_('email address'), unique=True)
+    last_name = models.CharField(_('姓'), max_length=150)
+    first_name = models.CharField(_('名'), max_length=150)
+    last_name_kana = models.CharField(_('姓（かな）'), max_length=150)
+    first_name_kana = models.CharField(_('名（かな）'), max_length=150)
+    old_last_name = models.CharField(_('旧姓'), max_length=150, blank=True, null=True)
+    old_last_name_kana = models.CharField(_('旧姓（かな）'), max_length=150, blank=True, null=True)
+    email = models.EmailField(_('メールアドレス'), unique=True)
+
+    sex = models.CharField(_('性別'), max_length=4, choices=(('男性','男性'), ('女性','女性')))
+    birthday = models.DateField(_('生年月日'), blank=True, null=True)
+
+    country = models.CharField(_('国'), default='日本', max_length=15, editable=False)
+    postal_code = models.CharField(_('郵便番号（ハイフンなし）'), max_length=7, blank=True, null=True)
+    prefecture = models.CharField(_('都道府県'), max_length=5, blank=True, null=True)
+    address = models.CharField(_('市区町村番地'), max_length=50, blank=True, null=True)
+    building = models.CharField(_('建物名'), max_length=30, blank=True, null=True)
+    tel = models.CharField(_('電話番号（ハイフンなし）'), max_length=11, blank=True, null=True)
     
+    url = models.URLField(_('URL'), max_length=300, blank=True, null=True)
+    photo = models.ImageField(_('写真'), blank=True, null=True)
+
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
@@ -84,46 +100,15 @@ class User(AbstractBaseUser, PermissionsMixin):
         self.email = self.__class__.objects.normalize_email(self.email)
 
     def get_full_name(self):
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = '%s %s' % (self.last_name, self.first_name)
         return full_name.strip()
 
+    def get_full_name_kana(self):
+        full_name_kana = '%s %s' % (self.last_name_kana, self.first_name_kana)
+        return full_name_kana.strip()
+    
     def get_short_name(self):
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         send_mail(subject, message, from_email, [self.email], **kwargs)
-
-    # class Meta:
-    #     db_table = 'User'
-    #     verbose_name_plural = 'ユーザのモデル'
-    
-    # uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    
-    # family_name = models.CharField(verbose_name = '姓', max_length=10)
-    # first_name = models.CharField(verbose_name = '名', max_length=10)
-    # family_name_kana = models.CharField(verbose_name = '姓（かな）', max_length=20)
-    # first_name_kana = models.CharField(verbose_name = '名（かな）', max_length=20)
-    # old_family_name = models.CharField(verbose_name = '旧姓', max_length=20, null=True)
-    # old_family_name_kana = models.CharField(verbose_name = '旧姓（かな）', max_length=20, null=True)
-    
-    # sex = models.CharField(verbose_name='性別', max_length=2, choices=(('男性','男性'), ('女性','女性')))
-    # birthday = models.DateTimeField(verbose_name='生年月日', null=True)
-    # email = models.EmailField(verbose_name='メールアドレス', max_length=100)
-    
-    # country = models.CharField(verbose_name='国', default='日本', max_length=15, editable=False)
-    # postal_code = models.SmallIntegerField(verbose_name='郵便番号（ハイフンなし）', null=True)
-    # prefecture = models.CharField(verbose_name = '都道府県', max_length=5, null=True)
-    # address = models.CharField(verbose_name = '住所', max_length=50, null=True)
-    # building = models.CharField(verbose_name = '建物名', max_length=30, null=True)
-    # tel = models.SmallIntegerField(verbose_name='電話番号（ハイフンなし）', null=True)
-    
-    # url = models.URLField(verbose_name='URL', max_length=300, null=True)
-    # photo = models.ImageField(verbose_name='写真', null=True)
-
-    # created_at = models.DateTimeField(default=timezone.now, editable=False)
-    # last_updated = models.DateTimeField(auto_now=True, editable=False)
-
-    # USERNAME_FIELD = 'email'
-
-    # def __str__(self):
-    #     return self.family_name + ' ' + self.first_name
